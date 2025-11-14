99290715321147da5ae8b11a23ba0ac374f3cf9e
Joao Ponte <joaoponte@MacBook-Pro-de-Joao-2.local>
2025-11-14 10:04:04 +0000

    fix: logs container

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-"0 6 * * *"}
 echo "Using CRON schedule: $CRON_SCHEDULE"
 
 # Create cron job
-echo "$CRON_SCHEDULE /app/script.sh >> /var/log/db-sync.log 2>&1" > /etc/cron.d/db-sync
+echo "$CRON_SCHEDULE /app/script.sh 2>&1" > /etc/cron.d/db-sync
+echo "" >> /etc/cron.d/db-sync
+
 chmod 0644 /etc/cron.d/db-sync
 
 touch /var/log/db-sync.log
